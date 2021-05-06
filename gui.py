c02d6f70597ef56e0a1cf679ed48a3d615f43b17
zanhp <zanuha007@gmail.com>
2021-05-06 19:21:12 +0200

    premik

---
+++ gui.py
@@ -0,0 +1,136 @@
+import os
+import time
+
+import arcade
+
+from smartdriver.constants import *
+from smartdriver.player import Player
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title, update_rate = UPDATE_RATE, smart=False, show=True):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__(width, height, title)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+        self.smart = smart
+        self.show = show
+
+        self.view_left = 0
+
+        arcade.Window.set_update_rate(self, update_rate)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Player(":resources:images/space_shooter/playerShip1_orange.png", SPRITE_SCALING, self.smart, self.show)
+        #self.player_sprite = Player(":resources:images/space_shooter/playerShip1_orange.png", SPRITE_SCALING, smart=True)
+        
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+        WHITE = (255,255,255)
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+        arcade.draw_line(150, 100, 3500, 100, WHITE, line_width=3)
+
+        # Draw all the sprites.
+        self.player_list.draw()
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        #print(self.player_list[0])
+        if self.player_list[0].smart:
+            self.player_list[0].next_move()
+        else:
+            self.player_list[0].update()
+
+        
+                
+        TRACK_WIDTH = 15
+        WHITE = (255,255,255)
+
+        main_points = ((100, 100), (2000, 200))
+        
+        
+        def track_points(point):
+            
+            return ((point[0], point[1] + TRACK_WIDTH), (point[0] , point[1] - TRACK_WIDTH))
+
+        main_points = list(map(track_points, main_points))
+
+        self.view_left += 2
+        
+        arcade.set_viewport(self.view_left, SCREEN_WIDTH + self.view_left, 0, SCREEN_HEIGHT)
+        #for i, element in enumerate(main_points[:-1]):
+        #    
+        #    arcade.draw_line(element[0][0], element[0][1], main_points[i][0][0], main_points[i][0][1], WHITE)
+        #    arcade.draw_line(element[0][0], element[0][1], main_points[i][0][0], main_points[i][0][1], WHITE)
+
+        #arcade.draw_line(150, 100, 350, 100, WHITE, line_width=3)
+
+        #points = list(range(100))
+        #points = list(zip(points,points)) 
+        #arcade.draw_points(points, color=(255,255,255))
+
+
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        if not self.player_list[0].smart:
+            # Forward/back
+            if key == arcade.key.UP:
+                self.player_sprite.on_press_key_up()
+            elif key == arcade.key.DOWN:
+                self.player_sprite.on_press_key_down()
+
+            # Rotate left/right
+            elif key == arcade.key.LEFT:
+                self.player_sprite.on_press_key_left()
+            elif key == arcade.key.RIGHT:
+                self.player_sprite.on_press_key_right()
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+        if not self.player_list[0].smart:
+            if key == arcade.key.UP:
+                self.player_sprite.on_release_key_up()
+            if key == arcade.key.DOWN:
+                self.player_sprite.on_release_key_down()
+            if key == arcade.key.LEFT:
+                self.player_sprite.on_release_key_left()
+            if key == arcade.key.RIGHT:
+                self.player_sprite.on_release_key_right()           
---
+++ player.py
@@ -0,0 +1,138 @@
+import math
+
+import arcade
+
+from smartdriver.constants import *
+
+
+class Player(arcade.Sprite):
+    """ Player class """
+    
+    def __init__(self, image, scale, smart=False, show=True):
+        """ Set up the player """
+
+        # Call the parent init
+        super().__init__(image, scale)
+        self.show = show
+        self.smart = smart
+
+        self.center_x = SCREEN_WIDTH / 2
+        self.center_y = SCREEN_HEIGHT / 2
+        self.center_x_noShow = self.center_x
+        self.center_y_noShow = self.center_y
+
+        self.speed_angle = 0
+        self.speed = 0
+
+        self.accelerating = False
+        self.braking = False
+
+    def __repr__(self):
+        return "smart: {}, up: {}, down: {}, right: {}, left: {}".format(self.smart, self.accelerating, self.braking, self.change_angle, self.change_angle)
+
+    def __str__(self):
+        #property_names=[p for p in dir(Player) if isinstance(getattr(Player,p),property)]
+        #return str(property_names)
+        return "smart: {}, up: {}, down: {}, right: {}, left: {}".format(self.smart, self.accelerating, self.braking, self.change_angle, self.change_angle)    
+
+    def print_self(self):
+        if self.show:            
+            print('x: {}, y: {}, v: {}, a: {}, sa: {}'.format(*list(map(lambda x : round(x,2),[self.center_x, self.center_y, self.speed, self.angle, self.speed_angle]))))
+        else:
+            print('x: {}, y: {}, v: {}, a: {}, sa: {}'.format(*list(map(lambda x : round(x,2),[self.center_x_noShow, self.center_y_noShow, self.speed, self.angle, self.speed_angle]))))
+
+    def update(self):
+        self.print_self()
+
+        if self.accelerating:
+            speed_temp = (self.speed + ACCELERATION_UNIT)
+            self.speed = min(speed_temp, MAX_SPEED)
+        elif self.braking:
+            speed_temp = (self.speed - ACCELERATION_UNIT * BRAKING_POWER) * FRICTION
+            self.speed = max(speed_temp, 0)
+        else:
+            speed_temp = self.speed * FRICTION
+            self.speed = speed_temp if speed_temp > TOL else 0
+
+        # Convert angle in degrees to radians.
+        #angle_rad = math.radians(self.angle)
+        #change_angle_rad = math.radians(self.change_angle)
+
+        # Rotate the ship
+        self.angle += self.change_angle        
+
+        # Use math to find our change based on our speed and angle
+        speed_angle_temp = (1 - (1-GRIP) * (self.speed / MAX_SPEED)**0.3) * (self.angle - self.speed_angle)
+        if abs(speed_angle_temp) > TOL_ANGLE:
+            self.speed_angle += speed_angle_temp    
+        #elif abs(speed_angle_temp) > TOL_ANGLE and self.change_angle == 0:
+            #print(0)
+            #self.speed_angle = self.angle
+        #else:
+
+        #self.speed_angle = self.speed_angle + speed_angle_temp if abs(speed_angle_temp) > TOL_ANGLE and self.change_angle else self.angle
+        speed_angle_rad = math.radians(self.speed_angle)
+
+        #speed_temp = (self.speed[0] - norm(self.speed) * math.sin(angle_rad + 5*change_angle_rad))
+
+        x_temp = (self.center_x if self.show else self.center_x_noShow) + self.speed * (-math.sin(speed_angle_rad))
+        y_temp = (self.center_y if self.show else self.center_y_noShow) + self.speed * math.cos(speed_angle_rad)
+        if x_temp < 0:
+            if self.show: 
+                self.center_x = 0
+            else:
+                self.center_x_noShow = 0
+        elif x_temp > SCREEN_WIDTH:
+            if self.show: 
+                self.center_x = SCREEN_WIDTH
+            else:
+                self.center_x_noShow = SCREEN_WIDTH
+        else:
+            if self.show:
+                self.center_x = x_temp
+            else:
+                self.center_x_noShow = x_temp
+        if y_temp < 0:
+            if self.show:
+                self.center_y = 0
+            else:
+                self.center_y_noShow = 0
+        elif y_temp > SCREEN_HEIGHT:
+            if self.show:
+                self.center_y = SCREEN_HEIGHT
+            else:
+                self.center_y_noShow = SCREEN_HEIGHT
+        else:
+            if self.show:
+                self.center_y = y_temp
+            else:
+                self.center_y_noShow = y_temp
+
+    def on_press_key_up(self):
+        self.accelerating = True
+
+    def on_press_key_down(self):
+        self.braking = True
+
+    def on_press_key_left(self):
+        self.change_angle = ANGLE_SPEED
+
+    def on_press_key_right(self):
+        self.change_angle = -ANGLE_SPEED
+
+    def on_release_key_up(self):
+        self.accelerating = False
+
+    def on_release_key_down(self):
+        self.braking = False
+
+    def on_release_key_left(self):
+        self.change_angle = 0
+
+    def on_release_key_right(self):
+        self.change_angle = 0
+
+    def next_move(self):
+        self.on_press_key_up()
+        self.update()
+    
